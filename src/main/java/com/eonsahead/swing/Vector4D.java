df4a97997d931f4a6f82a43f77151c13ec1bf5ae
Brian Cochran <bcbriancochran@gmail.com>
2020-04-06 22:17:25 -0500

    Added javadoc comments to Vector4D class

--- src/main/java/com/eonsahead/swing/Vector4D.java
+++ src/main/java/com/eonsahead/swing/Vector4D.java
@@ -1,16 +1,34 @@
 
 package com.eonsahead.swing;
 
-
+/**
+ * 
+ * @author Brian Cochran
+ * @version 4/6/2020
+ */
 public class Vector4D {
+    
     private double[] elements = new double[4];
     
+    /**
+     * Initializes a 4D vector to filled with zeroes.
+     */
     public Vector4D() {
         for (int i = 0; i < 4; i++) {
             this.set(i, 0);
         } // for
     } // Vector4D()
     
+    /**
+     * 
+     * Initializes a 4D vector using parameters.
+     * 
+     * Sets x, y, and z according to parameters and sets last value to 1.
+     * 
+     * @param x value to be placed in first spot
+     * @param y value to be placed in second spot
+     * @param z value to be placed in third spot
+     */
     public Vector4D(double x, double y, double z) {
         this.set(0, x);
         this.set(1, y);
@@ -18,14 +36,35 @@ public class Vector4D {
         this.set(3, 1);
     } // Vector4D()
     
+    /**
+     * 
+     * Retrieves the value at a specific location in a 4D vector.
+     * 
+     * @param row location of value to be retrieved
+     * @return value at specified location (double)
+     */
     public double get(int row) {
         return this.elements[row];
     } // get(int)
     
+    /**
+     * 
+     * Sets the value at a specific location in a 4D vector according to value parameter.
+     * 
+     * @param row location of value to be replaced
+     * @param value value to be set
+     */
     public void set(int row, double value) {
         this.elements[row] = value;
     } // set(int, double)
     
+    /**
+     * 
+     * Calculates dot product of two 4D vectors.
+     * 
+     * @param v Vector to be multiplied
+     * @return dot product (double)
+     */
     public double dotProduct(Vector4D v) {
         double product = 0.0;
         for (int i = 0; i < 4; i++) {
@@ -34,18 +73,46 @@ public class Vector4D {
         return product;
     } // dotProduct(Vector4D)
     
+    /**
+     * 
+     * Calculates magnitude of a 4D vector.
+     * 
+     * Uses dot product method to calculate the magnitude.
+     * 
+     * @return magnitude of 4D vector (double)
+     */
     public double magnitude() {
         return Math.sqrt(this.dotProduct(this));
     } // magnitude()
     
+    /**
+     * 
+     * Creates a new vector of magnitude 1 with a specific direction.
+     * 
+     * Takes direction of a 4D vector and sets the magnitude to 1 to create a new 4D vector.
+     * 
+     * @return 4D vector with magnitude of 1
+     */
     public Vector4D normalize() {
         double length = this.magnitude();
         double newX = this.get(0) / length;
         double newY = this.get(1) / length;
         double newZ = this.get(2) / length;
-        return new Vector4D(newX, newY, newZ);
+        double newQ = this.get(3) / length;
+        Vector4D result = new Vector4D(newX, newY, newZ);
+        result.set(3, newQ);
+        return result;
     } // normalize()
     
+    /**
+     * 
+     * Calculates the cross product of two 4D vectors.
+     * 
+     * Only uses first three spots of both vectors.
+     * 
+     * @param v 4D vector to be multiplied
+     * @return 4D vector
+     */
     public Vector4D crossProduct(Vector4D v) {
         double newX = this.get(1) * v.get(2) - this.get(2) * v.get(1);
         double newY = this.get(2) * v.get(0) - this.get(0) * v.get(2);
@@ -53,21 +120,14 @@ public class Vector4D {
         return new Vector4D(newX, newY, newZ);
     } // crossProduct(Vector4D)
     
+    /**
+     * 
+     * Converts a 4D vector object into a printable string.
+     * 
+     * @return string
+     */
     @Override
     public String toString() {
         return "(" + get(0) + ", " + get(1) + ", " + get(2) + ", " + get(3) + ")";
     } // toString()
-    
-    public static void main(String[] args) {
-        Matrix4x4 a = new Matrix4x4();
-        System.out.println(a);
-        
-        Vector4D b = new Vector4D(3, 3, 3);
-        System.out.println(b);
-        
-        Vector4D c = new Vector4D(4, 4, 4);
-        System.out.println(c);
-        
-        System.out.println(b.crossProduct(c));
-    } // main
-}
+} // Vector4D
