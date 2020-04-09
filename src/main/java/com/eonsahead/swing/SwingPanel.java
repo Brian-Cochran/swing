29543af3f07210ed3c570b3a2ec863015ec94faf
Brian Cochran <bcbriancochran@gmail.com>
2020-04-08 22:40:22 -0500

    feat: 3D object that rotates

--- src/main/java/com/eonsahead/swing/SwingPanel.java
+++ src/main/java/com/eonsahead/swing/SwingPanel.java
@@ -10,20 +10,18 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class SwingPanel extends JPanel implements ActionListener {
 
-    private final int points = 8;
     private double centerX = 0.0;
     private double centerY = 0.0;
     private final double minorRadius = 0.2;
     private final double majorRadius = 0.3;
 
-    private double deltaX = Math.random() / 20;
-    private double deltaY = Math.random() / 20;
-    private double deltaAngle = 2 * Math.PI / 180;
     private double phase = 0.0;
     private Shape shape1;
     private Shape shape2;
@@ -32,6 +30,8 @@ public class SwingPanel extends JPanel implements ActionListener {
     private Polygon3D poly1;
     private Polygon3D poly2;
     private Matrix4x4 spinner;
+    private List<Shape> shapes;
+    private Shape rect;
 
     public SwingPanel() {
         Timer timer = new Timer(20, this);
@@ -43,17 +43,17 @@ public class SwingPanel extends JPanel implements ActionListener {
 //        double r0 = this.minorRadius;
 //        double r1 = this.majorRadius;
 //        this.shape = makeStar(p, x, y, r0, r1);
-        this.poly1 = new Polygon3D(5, 0.6);
-        this.poly2 = new Polygon3D(5, 0.6);
+        this.poly1 = new Polygon3D(9, 0.6, 0);
+        this.poly2 = new Polygon3D(9, 0.6, 0.5);
         
         Matrix4x4 a = new Matrix4x4();
-        a.rotationX(Math.PI / 112);
+        a.rotationX(Math.PI / 100);
 
         Matrix4x4 b = new Matrix4x4();
-        b.rotationY(Math.PI / 144);
+        b.rotationY(Math.PI / 100);
 
         Matrix4x4 c = new Matrix4x4();
-        c.rotationZ(Math.PI / 80);
+        c.rotationZ(Math.PI / 100);
 
         this.spinner = a.multiply(b).multiply(c);
     } // SwingPanel()
@@ -84,12 +84,12 @@ public class SwingPanel extends JPanel implements ActionListener {
         scaling.setToScale(w / 2, h / 2);
 
         AffineTransform translation1 = new AffineTransform();
-        double cx1 = 1.0 + this.centerX;
-        double cy1 = 1.0 + this.centerY;
+        double cx1 = 1.0;
+        double cy1 = 1.0;
         translation1.setToTranslation(cx1, cy1);
         AffineTransform translation2 = new AffineTransform();
-        double cx2 = 0 + this.centerX;
-        double cy2 = 0 + this.centerY;
+        double cx2 = 1.0;
+        double cy2 = 1.0;
         translation2.setToTranslation(cx2, cy2);
 
         transform1.concatenate(scaling);
@@ -105,11 +105,22 @@ public class SwingPanel extends JPanel implements ActionListener {
 
         Shape s1 = transform1.createTransformedShape(this.shape1);
         Shape s2 = transform2.createTransformedShape(this.shape2);
-
+        
+        //Rectangle r = new Rectangle(poly2, poly1, 0);
+        //Shape shape3 = r.getShape();
+        //Shape s3 = transform1.createTransformedShape(shape3);
+        
         g2D.setColor(this.getColor());
-        g2D.draw(s1);
-        g2D.setColor(Color.red);
-        g2D.draw(s2);
+        g2D.fill(s1);
+        g2D.fill(s2);
+        //g2D.draw(s3);
+        
+        for (int side = 0; side < poly1.getSize(); side++) {
+            Rectangle r = new Rectangle(poly1, poly2, side);
+            Shape rectShape = r.getShape();
+            g2D.fill(transform1.createTransformedShape(rectShape));
+        } // for
+        
     } // paintComponent( Graphics )
 
     @Override
