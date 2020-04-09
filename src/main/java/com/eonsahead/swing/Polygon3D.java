dfcc014ad6adfbe5379e3ddaec37d4e646310c7c
Brian Cochran <bcbriancochran@gmail.com>
2020-04-09 00:47:08 -0500

    feat: Method that gets the minimum z coordinate of a polygon

--- src/main/java/com/eonsahead/swing/Polygon3D.java
+++ src/main/java/com/eonsahead/swing/Polygon3D.java
@@ -19,20 +19,29 @@ import java.awt.geom.GeneralPath;
 public class Polygon3D {
     private final List<Vector4D> vertices = new ArrayList<>();
 
-    public Polygon3D(int numberOfSides, double radius) {
+    public Polygon3D(int numberOfSides, double radius, double z) {
         for (int i = 0; i < numberOfSides; i++) {
             double fraction = ((double) i) / numberOfSides;
             double angle = fraction * 2.0 * Math.PI;
             double x = radius * Math.cos(angle);
             double y = radius * Math.sin(angle);
-            Vector4D v = new Vector4D(x, y, 0.0);
+            Vector4D v = new Vector4D(x, y, z);
             this.vertices.add(v);
+            System.out.println(this.vertices);
         } // for
     } // Polygon3D( int, double )
+    
+    public int getSize() {
+        return this.vertices.size() - 1;
+    } // getSize()
+    
+    public Vector4D getVertex(int vertex) {
+        return this.vertices.get(vertex);
+    } // getVertex(int)
 
     public void transform(Matrix4x4 m) {
         for (Vector4D u : this.vertices) {
-            u = m.multiply(u);
+            u.set(m.multiply(u));
         } // for 
     } // transform( Matrix )
 
@@ -54,5 +63,15 @@ public class Polygon3D {
         path.closePath();
 
         return path;
-    } // getShape()  
-}
+    } // getShape()
+    
+    public double getMinZ() {
+        double minZ = this.getVertex(0).get(2);
+        for (int vertex = 1; vertex < this.vertices.size(); vertex++) {
+            if (this.getVertex(vertex).get(2) < minZ) {
+                minZ = this.getVertex(vertex).get(2);
+            } // if
+        } // for
+        return minZ;
+    } // getMinZ()
+} // Polygon3D
