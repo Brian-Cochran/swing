e900844df61c7e384ded2e341885fa4b951aca19
Brian Cochran <bcbriancochran@gmail.com>
2020-04-09 00:47:25 -0500

    feat: Method that gets the minimum z coordinate of a rectangle

--- src/main/java/com/eonsahead/swing/Rectangle.java
+++ src/main/java/com/eonsahead/swing/Rectangle.java
@@ -9,26 +9,21 @@ public class Rectangle {
 
     private List<Vector4D> vertices = new ArrayList<>();
 
-    Rectangle(Polygon3D poly1, Polygon3D poly2, int i) {
-        
-    } // Rectangle(Polygon3D, Polygon3D, int)
-
-    public List<Vector4D> Rectangle(Polygon3D p1, Polygon3D p2, int side) {
+    public Rectangle(Polygon3D p1, Polygon3D p2, int side) {
         if (p1.getSize() == p2.getSize()) {
             if (side == p1.getSize()) {
                 this.vertices.add(p1.getVertex(side));
-                this.vertices.add(p1.getVertex(side));
                 this.vertices.add(p1.getVertex(0));
-                this.vertices.add(p1.getVertex(0));
+                this.vertices.add(p2.getVertex(0));
+                this.vertices.add(p2.getVertex(side));
             } // if
             else {
                 this.vertices.add(p1.getVertex(side));
-                this.vertices.add(p1.getVertex(side));
                 this.vertices.add(p1.getVertex(side + 1));
-                this.vertices.add(p1.getVertex(side + 1));
+                this.vertices.add(p2.getVertex(side + 1));
+                this.vertices.add(p2.getVertex(side));
             } // else
         } // if
-        return this.vertices;
     } // getPoints(Polygon3D, Polygon3D)
     
     public Shape getShape(){
@@ -40,7 +35,7 @@ public class Rectangle {
         path.moveTo(x, y);
         
         for (int i = 1; i < this.vertices.size(); i++) {
-            v = this.vertices.get(1);
+            v = this.vertices.get(i);
             x = v.get(0);
             y = v.get(1);
             path.lineTo(x, y);
@@ -48,4 +43,18 @@ public class Rectangle {
         path.closePath();
         return path;
     } // getShape()
+    
+    public Vector4D getVertex(int vertex) {
+        return this.vertices.get(vertex);
+    } // getVertex(int)
+    
+    public double getMinZ() {
+        double minZ = this.getVertex(0).get(2);
+        for (int vertex = 1; vertex < 4; vertex++) {
+            if (this.getVertex(vertex).get(2) < minZ) {
+                minZ = this.getVertex(vertex).get(2);
+            } // if
+        } // for
+        return minZ;
+    } // getMinZ()
 } // Rectangle
