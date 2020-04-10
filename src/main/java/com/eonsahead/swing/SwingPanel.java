26bcf35adad752c7fb2951bc894bca708c5c9360
Brian Cochran <bcbriancochran@gmail.com>
2020-04-10 12:16:16 -0500

    feat: can choose which shape to draw by changing value of shape variable

--- src/main/java/com/eonsahead/swing/SwingPanel.java
+++ src/main/java/com/eonsahead/swing/SwingPanel.java
@@ -17,28 +17,33 @@ public class SwingPanel extends JPanel implements ActionListener {
     private Color color = new Color(0, 255, 100);
     private final Matrix4x4 spinner;
     private final Vector4D illumination;
-    private final Polygon3D polyTop;
-    private final Polygon3D polyBottom;
-    private final int NUM_SIDES = 100;
+    private final Prism prism;
+    private final Cone cone;
+    private final int NUM_SIDES = 50;
+
+    // set to 1 for a prism, 2 for a cone, and 3 for an antiprism
+    private final int shape = 1;
 
     public SwingPanel() {
         Timer timer = new Timer(20, this);
         timer.start();
-        
-        this.polyTop = new Polygon3D(NUM_SIDES, 0.8, 0.5, 0);
-        this.polyBottom = new Polygon3D(NUM_SIDES, 0.0, -0.5, 1);
+
+        ///this.polyTop = new Polygon3D(NUM_SIDES, 0.8, 0.5, 0);
+        //this.polyBottom = new Polygon3D(NUM_SIDES, 0.0, -0.5, 1);
+        this.prism = new Prism(NUM_SIDES, 0.8, 0.8, 1);
+        this.cone = new Cone(0.8, 1);
 
         Matrix4x4 a = new Matrix4x4();
-        a.rotationX(Math.PI / 100);
+        a.rotationX(Math.PI / 400);
 
         Matrix4x4 b = new Matrix4x4();
-        b.rotationY(Math.PI / 100);
+        b.rotationY(Math.PI / 400);
 
         Matrix4x4 c = new Matrix4x4();
-        c.rotationZ(Math.PI / 100);
+        c.rotationZ(Math.PI / 400);
 
         this.spinner = a.multiply(b).multiply(c);
-        this.illumination = (new Vector4D(2, 2, 2)).normalize();
+        this.illumination = (new Vector4D(1, 1, 1)).normalize();
     } // SwingPanel()
 
     public Color getColor() {
@@ -55,9 +60,9 @@ public class SwingPanel extends JPanel implements ActionListener {
         int green;
         int blue;
         if (brightness > 0) {
-            red = (int) (brightness * c.getRed() * 1.5);
-            green = (int) (brightness * c.getGreen() * 1.5);
-            blue = (int) (brightness * c.getBlue() * 1.5);
+            red = (int) (brightness * c.getRed());
+            green = (int) (brightness * c.getGreen());
+            blue = (int) (brightness * c.getBlue());
         } // if
         else {
             red = (int) (ambient * c.getRed());
@@ -86,36 +91,54 @@ public class SwingPanel extends JPanel implements ActionListener {
         Vector4D normal;
         Shape shape;
 
-        shape = transform.createTransformedShape(polyTop.getShape());
-        normal = polyTop.getNormal();
-        if (normal.get(2) > 0) {
-            double brightness = normal.dotProduct(illumination);
-            g2D.setColor(chooseColor(brightness, ambient));
-            g2D.fill(shape);
+        if (this.shape == 1) {
+            for (Polygon3D face : this.prism.faces) {
+                shape = transform.createTransformedShape(face.getShape());
+                normal = face.getNormal();
+                if (normal.get(2) > 0) {
+                    double brightness = normal.dotProduct(illumination);
+                    g2D.setColor(chooseColor(brightness, ambient));
+                    g2D.fill(shape);
+                } // if
+            } // for
+            for (Rectangle side : this.prism.sides) {
+                shape = transform.createTransformedShape(side.getShape());
+                normal = side.getNormal();
+                if (normal.get(2) > 0) {
+                    double brightness = normal.dotProduct(illumination);
+                    g2D.setColor(chooseColor(brightness, ambient));
+                    g2D.fill(shape);
+                } // if
+            } // for
         } // if
-        shape = transform.createTransformedShape(polyBottom.getShape());
-        normal = polyBottom.getNormal();
-        if (normal.get(2) > 0) {
-            double brightness = normal.dotProduct(illumination);
-            g2D.setColor(chooseColor(brightness, ambient));
-            g2D.fill(shape);
-        } // if
-        for (int side = 0; side < polyTop.getSize() + 1; side++) {
-            Rectangle r = new Rectangle(polyTop, polyBottom, side);
-            shape = transform.createTransformedShape(r.getShape());
-            normal = r.getNormal();
-            if (normal.get(2) > 0) {
-                double brightness = normal.dotProduct(illumination);
-                g2D.setColor(chooseColor(brightness, ambient));
-                g2D.fill(shape);
-            } // if
-        } // for
+        else if (this.shape == 2) {
+            for (Polygon3D face : this.cone.faces) {
+                shape = transform.createTransformedShape(face.getShape());
+                normal = face.getNormal();
+                if (normal.get(2) > 0) {
+                    double brightness = normal.dotProduct(illumination);
+                    g2D.setColor(chooseColor(brightness, ambient));
+                    g2D.fill(shape);
+                } // if
+            } // for
+            for (Rectangle side : this.cone.sides) {
+                shape = transform.createTransformedShape(side.getShape());
+                normal = side.getNormal();
+                if (normal.get(2) > 0) {
+                    double brightness = normal.dotProduct(illumination);
+                    g2D.setColor(chooseColor(brightness, ambient));
+                    g2D.fill(shape);
+                } // if
+            } // for
+        } // else if
     } // paintComponent( Graphics )
 
     @Override
     public void actionPerformed(ActionEvent event) {
-        this.polyTop.transform(spinner);
-        this.polyBottom.transform(spinner);
+        //this.polyTop.transform(spinner);
+        //this.polyBottom.transform(spinner);
+        this.prism.transform(spinner);
+        this.cone.transform(spinner);
         this.repaint();
     } // actionPerformed( ActionEvent )
 } // SwingPanel
