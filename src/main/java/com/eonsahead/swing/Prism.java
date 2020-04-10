be75b7baaed30748a9c9328f57c02a8af31e9d68
Brian Cochran <bcbriancochran@gmail.com>
2020-04-10 11:33:05 -0500

    feat: Prism class creates a prism with defined top and bottom polygons

--- src/main/java/com/eonsahead/swing/Prism.java
+++ src/main/java/com/eonsahead/swing/Prism.java
@@ -5,6 +5,9 @@
  */
 package com.eonsahead.swing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  *
@@ -12,4 +15,30 @@ package com.eonsahead.swing;
  */
 public class Prism {
     
-}
+    public List<Polygon3D> faces = new ArrayList<>();
+    public List<Rectangle> sides = new ArrayList<>();
+    public List<Vector4D> vertices = new ArrayList<>();
+    
+    public Prism(int numSides, double radiusTop, double radiusBottom, double height) {
+        Polygon3D polyTop = new Polygon3D(numSides, radiusTop, height / 2, 0);
+        this.faces.add(polyTop);
+        this.vertices.addAll(polyTop.getVertices());
+        
+        Polygon3D polyBottom = new Polygon3D(numSides, radiusBottom, (-height) / 2, 1);
+        this.faces.add(polyBottom);
+        this.vertices.addAll(polyBottom.getVertices());
+        
+        Rectangle r;
+        for (int side = 0; side < numSides; side++) {
+            r = new Rectangle(polyTop, polyBottom, side);
+            sides.add(r);
+            this.vertices.addAll(r.getVertices());
+        } // for
+    } // Prism()
+    
+    public void transform(Matrix4x4 m) {
+        for (Vector4D v : this.vertices) {
+            v.set(m.multiply(v));
+        } // for
+    } // transform(Matrix4x4)
+} // Prism
