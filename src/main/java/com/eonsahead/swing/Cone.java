6876d05b3ea2a0b31213c287f01823418afdbe0b
Brian Cochran <bcbriancochran@gmail.com>
2020-04-10 12:03:41 -0500

    feat: Add Cone constructor and transform method

--- src/main/java/com/eonsahead/swing/Cone.java
+++ src/main/java/com/eonsahead/swing/Cone.java
@@ -5,10 +5,37 @@
  */
 package com.eonsahead.swing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Brian Cochran
  */
 public class Cone {
+    public List<Polygon3D> faces = new ArrayList<>();
+    public List<Rectangle> sides = new ArrayList<>();
+    public List<Vector4D> vertices = new ArrayList<>();
     
-}
+    public Cone(double radius, double height) {
+        Polygon3D polyTop = new Polygon3D(100, radius, height /  2, 0);
+        this.faces.add(polyTop);
+        this.vertices.addAll(polyTop.getVertices());
+        
+        Polygon3D polyBottom = new Polygon3D(100, 0.0, (-height) / 2, 1);
+        this.faces.add(polyBottom);
+        this.vertices.addAll(polyBottom.getVertices());
+        
+        Rectangle r;
+        for (int side = 0; side < 100; side++) {
+            r = new Rectangle(polyTop, polyBottom, side);
+            sides.add(r);
+        } // for
+    } // Cone(double, double)
+    
+    public void transform(Matrix4x4 m) {
+        for (Vector4D v : this.vertices) {
+            v.set(m.multiply(v));
+        } // for
+    } // transform(Matrix4x4)
+} // Cone
